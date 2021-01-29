2ee31723e91054a0804e91caefe353d2014ff7a9
sluck <sluck@student.ethz.ch>
2021-01-29 16:31:18 +0100

    add travis now

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 docker build -t sluck/multi-client:latest -t sluck/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
 docker build -t sluck/multi-server:latest -t sluck/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
 docker build -t sluck/multi-worker:latest -t sluck/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
-docker push sluck/multi-client:$GIT_SHA
-docker push sluck/multi-worker:$GIT_SHA
-docker push sluck/multi-server:$GIT_SHA
 
 docker push sluck/multi-client:latest
 docker push sluck/multi-worker:latest
 docker push sluck/multi-server:latest
+
+docker push sluck/multi-client:$GIT_SHA
+docker push sluck/multi-worker:$GIT_SHA
+docker push sluck/multi-server:$GIT_SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=sluck/multi-server:$GIT_SHA
